8f46169bd31cdec2d87be457df26b19175160841
Mizuking24 <mi_sama.ww1224@icloud.com>
2021-11-01 19:48:33 +0900

    [Finish]Java-4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "吉田瑞希";
+		System.out.println(name);
+		
+		name = "よろしくお願いします。";
+		System.out.println(name);
+		
+		final int RUCCY_NUMBER = 7;
+		System.out.println(RUCCY_NUMBER);
+	}
+
+}
